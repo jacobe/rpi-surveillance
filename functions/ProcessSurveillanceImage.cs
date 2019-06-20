cb23d687e30efc45fae6bf781e4326ee62f46965
Jacob Eisenberg <jaeisenb@microsoft.com>
2019-06-20 11:47:37 +0200

    Do image resizing with ImageSharp

--- functions/ProcessSurveillanceImage.cs
+++ functions/ProcessSurveillanceImage.cs
@@ -1,27 +1,50 @@
 using System;
 using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace RpiSurveillance.Functions
 {
     public static class ProcessSurveillanceImage
     {
+        const int WIDTH = 640, HEIGHT = 480;
+
         [FunctionName("ProcessSurveillanceImage")]
         public static async Task Run(
-            [BlobTrigger("pics/{name}")] Stream picStream,
-            [Blob("latest-pic/latest.jpg", FileAccess.Write)] Stream latestPicture, 
+            [BlobTrigger("pics/{name}")] CloudBlockBlob picture,
+            [Blob("latest-pic/latest.jpg", FileAccess.Write)] Stream output, 
             string name,
-            CancellationToken cancellationToken,
             ILogger log)
         {
             log.LogInformation($"C# Blob trigger function processed a request for file name {name}.");
 
-            await picStream.CopyToAsync(latestPicture, 4096, cancellationToken);
+            log.LogDebug("Resizing picture");
+            using (var memStream = new MemoryStream())
+            {
+                await picture.DownloadToStreamAsync(memStream);
+                memStream.Position = 0;
+                Resize(memStream, output);
 
-            log.LogInformation($"Latest picture uploaded: {name} ({picStream.Position} bytes)");
+                log.LogInformation($"Latest picture uploaded: {name} ({memStream.Position} bytes)");
+            }
+        }
+
+        private static void Resize(Stream input, Stream output)
+        {
+            var image = Image.Load(input);
+            image.Mutate(i =>
+                i.Resize(new ResizeOptions{
+                    Size = new SixLabors.Primitives.Size {
+                        Width = WIDTH,
+                        Height = HEIGHT
+                    },
+                    Mode = ResizeMode.Max
+                }));
+            image.SaveAsJpeg(output);
         }
     }
 }
